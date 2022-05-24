d1e8d26bc4d94adc691a94bfdc41bcffa4018537
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-05-24 09:18:19 -0300

    Initial commit

---
+++ Assets/playerControler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour {
+
+    public float zSpeed;
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) { 
+            transform.Translate(0, 0, zSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -zSpeed);
+        }
+        //if (Input.GetKey(KeyCode.A))
+       // {
+            //transform.position += new Vector3(-zSpeed, 0, 0);
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+            //transform.position += new Vector3(zSpeed, 0, 0);
+        //}
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles -= new Vector3(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+        }
+    }
+}
---
+++ Assets/proyectoHuveo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proyectoHuveo : MonoBehaviour {
+
+    public GameObject eggPrefab;
+    public GameObject eggSpawnPoint;
+    float tamano = 1; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(eggPrefab);
+            clon.transform.position = eggSpawnPoint.transform.position;
+            clon.transform.localScale *= 1;
+            tamano += 1; 
+        }
+	}
+}
